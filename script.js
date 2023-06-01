4fba94aea1b77da6427f97e4f0bbe6da6ca05727
JONATAS SOLCIA MACHADO <jonatas.smachado1@senacsp.edu.br>
2023-05-31 21:08:58 -0300

    modoescuro

---
+++ script.js
@@ -0,0 +1,9 @@
+/* var, let, const */
+/* const = constante => que não muda */
+
+const $html = document.querySelector('html');
+const $checkbox = document.querySelector('#switch');
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode');
+})
